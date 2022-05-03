f5536f84dc28300445c2c318bf6d12b101e95738
rodgallert <rodrigo.gallert96@gmail.com>
2022-05-03 03:57:56 -0300

    feat: implemented test for getFirst() method

--- spec/model/client.spec.js
+++ spec/model/client.spec.js
@@ -1,3 +1,5 @@
+const Client = require('../../model')
+
 describe('Clients', () => {
     beforeEach(()=>{
 
@@ -6,5 +8,12 @@ describe('Clients', () => {
     it('Return all clients', () => {
         let clients = Client.returnAll();
         expect(clients.length).toEqual(10);
-    })
+    });
+
+    it('Return first client', () =>{
+    let client = client.getFirst();
+    expect(client.id).not.toBeUndefined();
+    expect(client.name).not.toBeUndefined();
+    expect(client.phone).not.toBeUndefined();
+    });
 })
