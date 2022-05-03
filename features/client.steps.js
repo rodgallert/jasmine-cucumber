20436b4ff973d255ce25da3257686e033763c037
rodgallert <rodrigo.gallert96@gmail.com>
2022-05-03 06:53:44 -0300

    feat!: implemented steps for client. Not working properly

---
+++ features/client.steps.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+Given('I have {string} clients in the database', function (qty) {
+    return 'pending';
+});
+
+When('I access the home page', function () {
+    return 'pending';
+});
+
+Then('List {string} clients', function (qty) {
+    return 'pending';
+});
